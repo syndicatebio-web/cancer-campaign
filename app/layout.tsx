0d3184c5f85675c6de84f6cfb25e55e36f338dd6
tehin-elvis <elvissamuel8@gmail.com>
2026-03-23 18:46:40 +0100

    add embedded videos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import Script from 'next/script'
 
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
@@ -27,24 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P9CFHV8J');`}
-        </Script>
-      </head>
       <body className="font-sans antialiased">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P9CFHV8J"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
